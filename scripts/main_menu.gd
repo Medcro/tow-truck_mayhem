64d82531fc6a7d350c0be5323bdb23cc46451eb9
Medcro <muhammaddhiyarafi@gmail.com>
2025-11-21 07:58:59 +0700

    feat: add button sfx

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -19,6 +19,7 @@ func _ready():
 # handle start button
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn") # change scene to main
+	$ButtonPressed_SFX.play()
 
 func disabled_button() -> void:
 	start.disabled = true
@@ -30,23 +31,30 @@ func disabled_button() -> void:
 func _on_option_pressed() -> void:
 	disabled_button()
 	settings.visible = true
+	$ButtonPressed_SFX.play()
 
 # handle credits button
 func _on_credits_pressed() -> void:
+	$ButtonPressed_SFX.play()
 	pass
 
 # handle exit button
 func _on_exit_pressed() -> void:
 	disabled_button()
 	confirmation_exit.visible = true
+	$ButtonPressed_SFX.play()
 
 
 func _on_back_options_pressed() -> void:
 	_ready()
+	$ButtonPressed_SFX.play()
 
 func _on_yes_pressed() -> void:
 	get_tree().quit() # exit app
+	$ButtonPressed_SFX.play()
 
 
 func _on_no_pressed() -> void:
 	_ready()
+	$ButtonPressed_SFX.play()
+		
